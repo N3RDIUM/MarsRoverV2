abecd975986a089865431955ebfa3d2658184454
n3rdium <n3rdium@gmail.com>
2024-07-23 20:18:36 +0530

    s e r v o try13

--- src/rover.py
+++ src/rover.py
@@ -1,22 +1,19 @@
-from gpiozero.pins.pigpio import PiGPIOFactory
-PiGPIOFactory('127.0.0.1')
-
-from gpiozero import Servo
+from gpiozero import AngularServo
 from time import sleep
 
 # For the SG90 servo motor, the pulse width is 1ms to 2ms
-correction = 0.45
-minpulse = (1.0 - correction) / 1000
-maxpulse = (2.0 + correction) / 1000
-servo = Servo(16, min_pulse_width=minpulse, max_pulse_width=maxpulse, pin_factory=factory)
+# correction = 0.45
+# minpulse = (1.0 - correction) / 1000
+# maxpulse = (2.0 + correction) / 1000
+# servo = Servo(16, min_pulse_width=minpulse, max_pulse_width=maxpulse, pin_factory=factory)
 
-try:
-   while True:
-       servo.value = -1
-       sleep(1)
-       servo.value = 0
-       sleep(1)
-       servo.value = 1
-       sleep(1)
-finally:
-   servo.value = 0
+from gpiozero import AngularServo
+servo = AngularServo(18, min_angle=-42, max_angle=44)
+
+while True:
+    servo.min()
+    sleep(1)
+    servo.mid()
+    sleep(1)
+    servo.max()
+    sleep(1)
