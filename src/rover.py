dc6d1fe7f5dbe41e3534dad3477f66741f4d98e4
n3rdium <n3rdium@gmail.com>
2024-07-23 11:35:12 +0530

    s e r v o try2

--- src/rover.py
+++ src/rover.py
@@ -1,18 +1,30 @@
-print("Hello World from the Mars Rover V2!")
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(16, GPIO.OUT)
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
 
-# Set up the PWM on pin #16 at 50Hz
-pwm = GPIO.PWM(16, 50)
-pwm.start(0) # Start the servo with 0 duty cycle ( at 0 deg position )
-pwm.ChangeDutyCycle(5) # Tells the servo to turn to the left ( -90 deg position )
-sleep(0.5) # Tells the servo to Delay for 5sec
-pwm.ChangeDutyCycle(7.5) # Tells the servo to turn to the neutral position ( at 0 deg position )
-sleep(0.5) # Tells the servo to Delay for 5sec
-pwm.ChangeDutyCycle(10) # Tells the servo to turn to the right ( +90 deg position )
-sleep(0.5) # Tells the servo to Delay for 5sec
-pwm.stop(0) # Stop the servo with 0 duty cycle ( at 0 deg position )
-GPIO.cleanup() # Clean up all the ports we've used.
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
