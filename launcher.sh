fd4f1c62d5434520ebe217d5a1be804610548308
n3rdium <n3rdium@gmail.com>
2024-07-23 09:16:20 +0530

    Added the boilerplate stuff

---
+++ launcher.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+git pull
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
+python3 src/client.py
---
+++ src/client.py
@@ -0,0 +1,1 @@
+print("Hello World from the Client code for the Mars Rover V2!")
